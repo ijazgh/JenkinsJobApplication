77a9b9496cf93cb9b9d8abbc25e3d2f4516b0ac8
ijazgh <ijazchoudary@hotmail.com>
2025-02-18 01:41:23 -0500

    Initial commit

---
+++ src/main/java/com/chtech/JenkinsJobApplication/JenkinsJobApplication.java
@@ -0,0 +1,22 @@
+package com.chtech.JenkinsJobApplication;
+
+import jakarta.annotation.PostConstruct;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class JenkinsJobApplication {
+
+	public static Logger logger = (Logger)	LogFactory.getLog(JenkinsJobApplication.class);
+    @PostConstruct
+	public void init(){
+		logger.info("Welome to JenkinsJobApplication!");
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsJobApplication.class, args);
+	}
+
+}
